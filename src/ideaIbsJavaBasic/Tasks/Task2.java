6cbdd3ff53fa3f0a38775d5e5cc7f6c5da2cb21f
wowLiu <liubovff@gmail.com>
2023-02-09 23:58:13 +0300

    Задание 2

---
+++ src/ideaIbsJavaBasic/Tasks/Task2.java
@@ -0,0 +1,10 @@
+package ideaIbsJavaBasic.Tasks;
+
+public class Task2 {
+    public static void main(String[] args)
+    {
+        double a = 56.5650;
+        byte c = 1;
+        System.out.println(a+" "+c);
+    }
+}
