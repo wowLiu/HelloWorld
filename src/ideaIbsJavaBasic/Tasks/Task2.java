4a2641e916566c2c6f70f4bf9ca4b649916113cf
wowLiu <liubovff@gmail.com>
2023-02-10 00:00:40 +0300

    Задание 2

--- src/ideaIbsJavaBasic/Tasks/Task2.java
+++ src/ideaIbsJavaBasic/Tasks/Task2.java
@@ -4,7 +4,7 @@ public class Task2 {
     public static void main(String[] args)
     {
         double a = 56.5650;
-        byte c = 1;
+        byte c = 127;
         System.out.println(a+" "+c);
     }
 }
